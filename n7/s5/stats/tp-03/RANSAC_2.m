d25ee3b7258a445bffba08891ca8d01d70f5d0f5
seba12 <seba12.04@orange.fr>
2021-02-01 21:55:11 +0100

    fixing Stats TP 3

--- n7/s5/stats/tp-03/RANSAC_2.m
+++ n7/s5/stats/tp-03/RANSAC_2.m
@@ -1,9 +1,8 @@
 function [rho_F, theta_F] = RANSAC_2(rho, theta, parametres)
+    n = length(rho);
     S1 = parametres(1);
     S2 = parametres(2);
     k_max = parametres(3);
-
-    n = length(rho);
 
     m_max = 0;
     rho_F = 1;
@@ -13,21 +12,25 @@ function [rho_F, theta_F] = RANSAC_2(rho, theta, parametres)
 
         % On choisi deux points au hasard
         I = randperm(n, 2);
-        nouveau_rho = [rho(I(1)) rho(I(2))];
-        nouveau_theta = [theta(I(1)) theta(I(2))];
+        nouveau_rho = [rho(I(1)); rho(I(2))];
+        nouveau_theta = [theta(I(1)); theta(I(2))];
 
         % On estime les paramètres à partir de ces points
         [rho_1, theta_1] = estimation_F(nouveau_rho, nouveau_theta);
 
-        % On test les autres données
-        C = rho - rho_1 * cos(theta - theta_1) <= S1;
-        p = sum(C) / n;
+        % On teste si ce modèle colle aux autres points
+        C = abs(rho - rho_1 * cos(theta - theta_1)) <= S1;
+        p = sum(C) / (n - 1);
 
-        if p >= S2
-            rho_conforme = rho(C);
-            theta_conforme = theta(C);
-            [rho_2, theta_2, m] = estimation_F_bis(rho_conforme, theta_conforme);
+        if p > S2
 
+            % On réestime le modèle avec tous les points qui collent
+            [rho_2, theta_2] = estimation_F(rho(C), theta(C));
+
+            % On calcule la moyenne des écarts
+            m = sum(abs(rho(C) - rho_2 * cos(theta(C) - theta_2)));
+
+            % On sélectionne la plus grande
             if m >= m_max
                 rho_F = rho_2;
                 theta_F = theta_2;
